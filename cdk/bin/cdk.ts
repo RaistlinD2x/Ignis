1346a3379de7fe4d252b85e15d3af7ce51999060
Node-Jesse-Richey <jesse.richey@node.digital>
2024-10-20 08:01:48 -0500

    added boostrapping logic to exclude secretstack synth when needed

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,7 +3,19 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from '../lib/PipelineStack';
 import { SecretStack } from '../lib/SecretStack';
+import * as fs from 'fs';
 
 const app = new cdk.App();
+
+// Check if bootstrapping is needed based on the presence of .env file or an environment variable
+const isBootstrapping = fs.existsSync('.env');
+
+if (isBootstrapping) {
+  console.log('Bootstrapping: Deploying SecretStack');
+  // Deploy the SecretStack only if bootstrapping is required
+  new SecretStack(app, 'SecretStack', {});
+} else {
+  console.log('Skipping SecretStack as it is not needed.');
+}
+
 new PipelineStack(app, 'PipelineStack', {});
-new SecretStack(app, 'SecretStack', {});
