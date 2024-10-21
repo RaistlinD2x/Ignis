4c9613723465703cc398b0c103892f70ec5a405f
Node-Jesse-Richey <jesse.richey@node.digital>
2024-10-20 21:11:26 -0500

    testing configPath resolution

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -10,6 +10,7 @@ import * as path from 'path';
 
 // TODO: move this to a helper function
 const configPath = path.resolve(__dirname, 'config.yaml');
+console.log(configPath)
 const config: any = yaml.load(fs.readFileSync(configPath, 'utf8'));
 const configEnvironments = config.environments
 
